42002b1e4bd4530be4d52a3111c1405a35065fd0
shhx <luisgomezlo.lg@gmail.com>
2022-12-24 17:23:41 +0100

    Day 24 finished

--- src/bin/day24_2.py
+++ src/bin/day24_2.py
@@ -20,7 +20,7 @@ for y, line in enumerate(board):
         elif char == "v":
             bliz_list.append((x, y, 0, 1))
 bliz = {}
-for t in range(1000):
+for t in range(800):
     new_bliz = set()
     for x, y, dx, dy in bliz_list:
         x = (x-1 + dx * t) % (W-2) + 1
@@ -45,13 +45,26 @@ def print_board(i, j, t):
         print()
 
 seen = set()
-start = (1, 0, 0)
-to_visit = deque([start])
-end = (W-2, L-1)
+start = (1, 0)
+to_visit = deque([(start[0], start[1], 0)])
+iend = (W-2, L-1)
+end = iend
+ride = 0
 while True:
     x,y,step = to_visit.popleft()
     if (x,y,step) in seen:
         continue
+    if (x, y) == end:
+        print("Found", step-1)
+        seen.clear()
+        to_visit.clear()
+        start, end = end, start
+        to_visit.append((start[0], start[1], step-1))
+        print("Going back: ", ride, step-1)
+        ride += 1
+        if ride == 3:
+            print("Found", step-1)
+            break
     # print(x, y, step)
     # print_board(x, y, step)
     seen.add((x,y,step))
@@ -61,11 +74,10 @@ while True:
         xn = x + dir[0]
         yn = y + dir[1]
         if (xn, yn) == end:
-            print("Found", step)
-            exit()
+            to_visit.append((xn, yn, step+1))
         if xn >= 1 and xn < W-1 and yn >= 1 and yn < L-1 and (xn,yn) not in bliz[step]:
             to_visit.append((xn, yn, step+1))
-    if len(seen) % 1000 == 0:
-        print(len(seen), step)
+    # if len(seen) % 1000 == 0:
+    #     print(len(seen), step)
 
 # 247
